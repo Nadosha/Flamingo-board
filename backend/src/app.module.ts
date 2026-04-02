0f15400232580143b9124df295ca1db80a8dde35
Andrey Nadosha <andrey.nadosha@gmail.com>
2026-04-02 09:20:00 +0200

    chore: scaffold NestJS backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { BoardsModule } from './boards/boards.module';
+import { ColumnsModule } from './columns/columns.module';
+import { CardsModule } from './cards/cards.module';
+import { LabelsModule } from './labels/labels.module';
+import { RealtimeModule } from './realtime/realtime.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UsersModule,
+    WorkspacesModule,
+    BoardsModule,
+    ColumnsModule,
+    CardsModule,
+    LabelsModule,
+    RealtimeModule,
+  ],
+})
+export class AppModule {}
---
+++ backend/src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, transform: true, forbidNonWhitelisted: true }),
+  );
+
+  app.use(cookieParser());
+  app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  const port = process.env.PORT ?? 4000;
+  await app.listen(port);
+  console.log(`Backend running on http://localhost:${port}/api`);
+}
+bootstrap();
